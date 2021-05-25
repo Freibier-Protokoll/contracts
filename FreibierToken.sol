ba4f1e3756801e77e2831e7870b02a1f4d1f5507
Sono Willbrand <sono00@web.de>
2021-05-25 16:54:09 +0200

    Fix functionality

--- FreibierToken.sol
+++ FreibierToken.sol
@@ -1,18 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-contract FreibierToken is ERC20Capped, Ownable {
-    
-    constructor(uint256 initialBalance, uint256 cap) ERC20("Freibier Token", "Freibier") ERC20Capped(cap * 10 ** decimals()) {
-        require(initialBalance <= cap, "CommonERC20: cap exceeded"); // this is needed to know for sure the cap is not exceded.
-        ERC20._mint(msg.sender, initialBalance * 10 ** decimals());
+contract FreibierToken is ERC20, Ownable {
+    constructor(uint256 initialBalance) ERC20("Freibier Token", "Freibier") {
+        ERC20._mint(msg.sender, initialBalance * 10**decimals());
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
-    
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override {
+        super._beforeTokenTransfer(from, to, amount);
+        mint(from, amount);
+    }
 }
