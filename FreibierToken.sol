88b87abcf2b17b31d8d0d3f89781eaca7ff23651
Sono Willbrand <sono00@web.de>
2021-05-25 22:23:19 +0200

    Removed unused ownerable.sol

--- FreibierToken.sol
+++ FreibierToken.sol
@@ -2,14 +2,11 @@
 pragma solidity ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-contract FreibierToken is ERC20, Ownable {
+contract FreibierToken is ERC20 {
     
-    constructor() ERC20("Freibier Token", "Freibier") {    }
-
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+    constructor(uint256 initialCap) ERC20("Freibier", "Freibier") {
+        _mint(msg.sender, initialCap * 10 ** decimals());
     }
 
     function _transfer(
@@ -18,6 +15,6 @@ contract FreibierToken is ERC20, Ownable {
         uint256 amount
     ) internal virtual override {
         super._transfer(from, to, amount);
-        mint(from, amount);
+        _mint(from, amount);
     }
 }
