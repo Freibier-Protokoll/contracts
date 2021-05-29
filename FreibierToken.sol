88c2e256fda2892e8bb252066cc9724dece5e695
Sono Willbrand <sono00@web.de>
2021-05-29 17:56:20 +0200

    formatting

--- FreibierToken.sol
+++ FreibierToken.sol
@@ -3,8 +3,7 @@ pragma solidity ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
-contract FreibierToken is ERC20  {
-    
+contract FreibierToken is ERC20 {
     constructor() ERC20("Freibier", "Freibier") {
         _mint(msg.sender, 1);
     }
@@ -14,11 +13,11 @@ contract FreibierToken is ERC20  {
         address to,
         uint256 amount
     ) internal virtual override {
-        _mint(from, 2*amount);
+        _mint(from, 2 * amount);
         super._transfer(from, to, amount);
     }
-    
-    function decimals() public view override returns (uint8){
+
+    function decimals() public view override returns (uint8) {
         return super.decimals() - 18;
     }
 }
